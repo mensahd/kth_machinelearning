69920e1b293ef6864511796885788fca0a91a249
mensahd <daniel.mensah@tum.de>
2019-09-13 11:29:42 +0200

    Implementation of Assigment 1

--- Lab_1/dectrees/python/decisiontree_solver.py
+++ Lab_1/dectrees/python/decisiontree_solver.py
@@ -1,6 +1,30 @@
-import monkdata as m
+from Lab_1.dectrees.python import monkdata as m
+from Lab_1.dectrees.python import dtree
 
-class DecTreeSolver:
-  def __init__(self, number=1):
-    self.monkset = number
 
+
+def getMonkset(number):
+    monknumber = 'monk' + str(number)
+    return getattr(m, monknumber, lambda: "Invalid MONK dataset")
+
+def assignment1_daniel():
+    print("Assignment 1:")
+    for i in range(1, 4):
+        print("MONK-" + str(i) + " entropy is:" + str(dtree.entropy(getMonkset(i))))
+
+def assignment2_daniel():
+    print("")
+
+def assignment3_daniel():
+    print("")
+
+def assignment4_daniel():
+    print("")
+
+def assignment5_daniel():
+    print("")
+
+def run_daniel():
+  assignment1_daniel()
+
+run_daniel()
