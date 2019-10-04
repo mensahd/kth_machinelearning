8aa16efd8493430af742bc31de177f2eab7810bc
mensahd <daniel.mensah@tum.de>
2019-10-04 10:52:10 +0200

    Initial Commit for Lab2

---
+++ Lab_2/SVM_solver.py
@@ -0,0 +1,10 @@
+import numpy , random , math
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+
+
+numpy.zeros(N)
+
+ret = minimize(objective, start, bounds= B, constraints=XC)
+alpha = ret[’x’]
